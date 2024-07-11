541220af4fb9c966fa80fda90e06b7f595b1c7fc
Muneesh <muneeshkumar405@gmail.com>
2024-07-12 04:09:30 +0530

    completed

---
+++ components/CountriesList.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import CountryCard from './CountryCard'
+import CountriesListShimmer from './CountriesListShimmer'
+
+export default function CountriesList({ query }) {
+  const [countriesData, setCountriesData] = useState([])
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => res.json())
+      .then((data) => {
+        setCountriesData(data)
+      })
+  }, [])
+
+  if (!countriesData.length) {
+    return <CountriesListShimmer />
+  }
+
+  return (
+    <>
+      <div className="countries-container">
+        {countriesData.sort((a, b) => {
+    if (a.name.common < b.name.common) {
+        return -1;
+    }
+    if (a.name.common > b.name.common) {
+        return 1;
+    }
+    return 0;
+}).filter((country) =>
+            country.name.common.toLowerCase().includes(query) || country.region.toLowerCase().includes(query)
+          )
+          .map((country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                name={country.name.common}
+                flag={country.flags.svg}
+                population={country.population}
+                region={country.region}
+                capital={country.capital?.[0]}
+                data={country}
+              />
+            )
+          })}
+      </div>
+    </>
+  )
+}
